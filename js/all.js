e5f2155259bc111a0f13db79c766a62fc8c3d9bf
zhaoyulong@air-cooking.com <zhaoyulong@air-cooking.com>
2018-06-01 17:20:49 +0800

    doctor

---
+++ js/all.js
@@ -0,0 +1,66 @@
+//swiper
+//index swiperone
+var mySwiper = new Swiper('.swiper-indexone', {
+	prevButton: '.swiper-button-prev',
+	nextButton: '.swiper-button-next',
+})
+//migrantoryBird swipertwo
+var birdSwiper = new Swiper('.migrantory-swiper.swiper-container', {
+	prevButton: '.swiper-button-prev',
+	nextButton: '.swiper-button-next',
+	pagination: '.swiper-pagination',
+	//autoplay: 3000,
+	loop: true,
+	slidesPerView: 3,
+	centeredSlides: true,
+	uniqueNavElements: false,
+	speed: 1000
+})
+//migrantoryA 
+//获取li						
+var liList = $('.migrantTab li');
+//触发遮罩层
+$('.secondnav').on('click', 'li', function() {
+	console.log($(this).index())
+	$(this).addClass('li-hover').siblings().removeClass('li-hover')
+	var currentIndex = $(this).index();
+	liList.eq(currentIndex).show().siblings().hide();
+
+})
+
+
+//homevist
+var homeList = $('.theright li');
+		
+		var homeSwiper = new Swiper('.swiper-container.homevisitSwiper', {
+			
+			//autoplay:3000,
+			direction: 'vertical',
+			
+			speed:1000,
+			onSlideChangeEnd: function(swiper) {
+				console.log(swiper.activeIndex);
+				homeList.eq(swiper.activeIndex).addClass('li-hover').siblings().removeClass('li-hover')
+			}
+
+		})
+
+		//触发遮罩层包括三角
+		$('.theright').on('click', 'li', function() {
+			console.log($(this).index())
+			$(this).addClass('li-hover').siblings().removeClass('li-hover')
+			var currentIndex = $(this).index();
+			homeSwiper.slideTo(currentIndex, 1000, false)
+		})
+$(function() {
+	//headComponents  headhover
+	var aClick = sessionStorage.getItem('aClick');
+	if(aClick) {
+		$('.head-span').eq(aClick).addClass('head-active');
+	}
+	$('.head-nav').on('click', '.head-span', function() {
+		console.log($(this).data('id'));
+		sessionStorage.setItem('aClick', $(this).data('id'))
+	})
+
+})
---
+++ readme.txt
@@ -0,0 +1,18 @@
+find one 自动补全兼容插件 gulp
+
+目录结构
+ 
+  共用组件
+	  head.html
+	  foot.html
+  各页对应
+	   首页 index
+	   公司介绍 introduction
+	   候鸟计划 migrantoryBird
+	   旅居-情怀migrantoryA
+	   旅居-颐养migrantoryB(同A)
+   	   居家探访 homeVisit
+   	   居家A homeVisitA(基本同miA)
+   	   居家B homeVisitB(基本同miA)
+   	   人员介绍 staffInfo
+   	   养老产品 product
